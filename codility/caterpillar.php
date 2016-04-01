6d2069d88953f2e1fdfbc44771bcec36de0dc348
eldarbabayev <eldarbabayev@users.noreply.github.com>
2016-04-02 02:28:24 +0500

    Update caterpillar.php

--- codility/caterpillar.php
+++ codility/caterpillar.php
@@ -4,18 +4,21 @@
 function solution($A) {
     // write your code in PHP5.5
     $begin = 0;
-    $end = length($A);
+    $end = count($A)-1;
     $answer = 0;
-    for ($i = 1; $i < length($A)-1; $i++) {
-    	if (abs($A[$begin]) < abs($A[$end])) {
-	   $begin += 1;
-	} else if (abs($A[$begin]) = abs($A[$end])) {
-	  $answer += 1;
-	  $begin += 1;
-	} else {
-	  $end -= 1;
-	}
+    for ($i = 1; $i < count($A); $i++) {
+        $m = $A[$begin];
+        $n = $A[$end];
+        if (abs($m) < abs($n)) {
+            $end -= 1;
+            $answer += 1;
+        } else if (abs($m) == abs($n)) {
+            $begin += 1;
+        } else {
+            $begin += 1;
+            $answer += 1;
+        }
     }
-
-    return $answer;
+    
+    return $answer + 1;
 }
