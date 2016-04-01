faab99dd766d3c7cb8095af294bc701fdbba0026
eldarbabayev <eldarbabayev@users.noreply.github.com>
2016-04-02 02:28:57 +0500

    Update caterpillar.php

--- codility/caterpillar.php
+++ codility/caterpillar.php
@@ -1,8 +1,6 @@
-// you can write to stdout for debugging purposes, e.g.
-// print "this is a debug message\n";
+// 50% correctness, need improvement
 
 function solution($A) {
-    // write your code in PHP5.5
     $begin = 0;
     $end = count($A)-1;
     $answer = 0;
